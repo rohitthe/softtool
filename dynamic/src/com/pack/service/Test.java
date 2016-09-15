33ad57d4eb31cf89565b6fffec984a93ad48bb6b
rohit.kumar63 <rohit.kumar63@CHDSEZ384514D.ad.infosys.com>
2016-09-15 16:17:19 +0530

    Its second message

--- dynamic/src/com/pack/service/Test.java
+++ dynamic/src/com/pack/service/Test.java
@@ -7,13 +7,12 @@ import com.pack.service.Service;
 public class Test {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
           try {
 			Service.sendGET("http://localhost:8080//api/json?tree=jobs[name,url,builds[number,url]]");
 			Service.ServiceCall("http://localhost:8080/createItem?name=sarthak", "<project>  <description></description>  <keepDependencies>false</keepDependencies>  <properties/> <scm class='hudson.scm.NullSCM'/>  <canRoam>true</canRoam>  <disabled>false</disabled>  <blockBuildWhenDownstreamBuilding>false</blockBuildWhenDownstreamBuilding>  <blockBuildWhenUpstreamBuilding>false</blockBuildWhenUpstreamBuilding>  <triggers/>  <concurrentBuild>false</concurrentBuild>  <builders>    <hudson.tasks.Maven>      <targets>package</targets>      <usePrivateRepository>false</usePrivateRepository>      <settings class='jenkins.mvn.DefaultSettingsProvider'/>      <globalSettings class='jenkins.mvn.DefaultGlobalSettingsProvider'/>    </hudson.tasks.Maven>  </builders>  <publishers/>  <buildWrappers/></project>");
 			Service.deleteJob("sarthak2","http://localhost:8080");
+			System.out.println("rohit kumar");
           } catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
           }
 	}
