6cbef80b2aab156d19363eb7853b922680442b27
rohit.kumar63 <rohit.kumar63@CHDSEZ384514D.ad.infosys.com>
2016-09-15 16:18:58 +0530

    thirrdcommet

--- dynamic/src/com/pack/service/Test.java
+++ dynamic/src/com/pack/service/Test.java
@@ -12,6 +12,7 @@ public class Test {
 			Service.ServiceCall("http://localhost:8080/createItem?name=sarthak", "<project>  <description></description>  <keepDependencies>false</keepDependencies>  <properties/> <scm class='hudson.scm.NullSCM'/>  <canRoam>true</canRoam>  <disabled>false</disabled>  <blockBuildWhenDownstreamBuilding>false</blockBuildWhenDownstreamBuilding>  <blockBuildWhenUpstreamBuilding>false</blockBuildWhenUpstreamBuilding>  <triggers/>  <concurrentBuild>false</concurrentBuild>  <builders>    <hudson.tasks.Maven>      <targets>package</targets>      <usePrivateRepository>false</usePrivateRepository>      <settings class='jenkins.mvn.DefaultSettingsProvider'/>      <globalSettings class='jenkins.mvn.DefaultGlobalSettingsProvider'/>    </hudson.tasks.Maven>  </builders>  <publishers/>  <buildWrappers/></project>");
 			Service.deleteJob("sarthak2","http://localhost:8080");
 			System.out.println("rohit kumar");
+			System.out.println("rohit");
           } catch (IOException e) {
 			e.printStackTrace();
           }
